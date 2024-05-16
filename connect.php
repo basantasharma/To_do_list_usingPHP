d4c18cf725e9ed6faa0187e9cfd698795d3db958
basantasharma <gyawalibasanta0@gmail.com>
2024-05-16 21:11:16 +0545

    completed todo app

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $serverName = "localhost";
 $username = "root";
 $password = "";
-$dbname = "validation";
+$dbname = "todo_app";
 
 $conn = mysqli_connect($serverName, $username, $password, $dbname);
 if(!$conn)
